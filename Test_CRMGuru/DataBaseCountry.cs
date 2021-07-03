5d0e41e5b791ed65ed572d6d9d578ed30c6dee17
juliapopova <yliana97@mail.ru>
2021-07-03 23:09:37 +0300

    Подключение к БД

---
+++ Test_CRMGuru/DataBaseCountry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Test_CRMGuru
+{
+    class DataBaseCountry
+    {
+        private SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["connectionCountry"].ConnectionString);
+    }
+}
