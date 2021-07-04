f08b4533f4f8b53325caf597f568338a73f6e4dc
juliapopova <yliana97@mail.ru>
2021-07-04 15:34:21 +0300

    создание класса country и запись стран в БД

---
+++ Test_CRMGuru/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_CRMGuru
+{
+    class Country
+    {
+        public string name { get; set; }
+        public string alpha2Code { get; set; }
+        public string capital { get; set; }
+        public string area { get; set; }
+        public string population { get; set; }
+        public string region { get; set; }
+    }
+}
