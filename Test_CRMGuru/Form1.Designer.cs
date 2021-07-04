3a28d87d14463796ea091021c1c5058e7bdee09e
juliapopova <yliana97@mail.ru>
2021-07-04 10:44:26 +0300

    Интерфейс

--- Test_CRMGuru/Form1.Designer.cs
+++ Test_CRMGuru/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Test_CRMGuru
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -29,13 +29,105 @@ namespace Test_CRMGuru
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridViewCountry = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox_Country = new System.Windows.Forms.TextBox();
+            this.but_SearchCountry = new System.Windows.Forms.Button();
+            this.but_LoadFromBD = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCountry)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridViewCountry
+            // 
+            this.dataGridViewCountry.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCountry.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewCountry.Location = new System.Drawing.Point(3, 75);
+            this.dataGridViewCountry.Name = "dataGridViewCountry";
+            this.dataGridViewCountry.Size = new System.Drawing.Size(1241, 523);
+            this.dataGridViewCountry.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridViewCountry, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1247, 601);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.but_LoadFromBD);
+            this.panel1.Controls.Add(this.but_SearchCountry);
+            this.panel1.Controls.Add(this.textBox_Country);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1241, 66);
+            this.panel1.TabIndex = 4;
+            // 
+            // textBox_Country
+            // 
+            this.textBox_Country.ForeColor = System.Drawing.Color.Gray;
+            this.textBox_Country.Location = new System.Drawing.Point(9, 9);
+            this.textBox_Country.Name = "textBox_Country";
+            this.textBox_Country.Size = new System.Drawing.Size(280, 20);
+            this.textBox_Country.TabIndex = 0;
+            this.textBox_Country.Text = "Введите название страны";
+            this.textBox_Country.Click += new System.EventHandler(this.textBox_Country_Click);
+            this.textBox_Country.Leave += new System.EventHandler(this.textBox_Country_Leave);
+            // 
+            // but_SearchCountry
+            // 
+            this.but_SearchCountry.Location = new System.Drawing.Point(295, 8);
+            this.but_SearchCountry.Name = "but_SearchCountry";
+            this.but_SearchCountry.Size = new System.Drawing.Size(147, 22);
+            this.but_SearchCountry.TabIndex = 1;
+            this.but_SearchCountry.Text = "Найти страну";
+            this.but_SearchCountry.UseVisualStyleBackColor = true;
+            // 
+            // but_LoadFromBD
+            // 
+            this.but_LoadFromBD.Location = new System.Drawing.Point(9, 36);
+            this.but_LoadFromBD.Name = "but_LoadFromBD";
+            this.but_LoadFromBD.Size = new System.Drawing.Size(433, 23);
+            this.but_LoadFromBD.TabIndex = 2;
+            this.but_LoadFromBD.Text = "Вывести все страны из базы данных";
+            this.but_LoadFromBD.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1247, 601);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FormMain";
+            this.Text = "Информация о странах";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCountry)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView dataGridViewCountry;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button but_LoadFromBD;
+        private System.Windows.Forms.Button but_SearchCountry;
+        private System.Windows.Forms.TextBox textBox_Country;
     }
 }
 
--- Test_CRMGuru/Form1.cs
+++ Test_CRMGuru/Form1.cs
@@ -10,11 +10,29 @@ using System.Windows.Forms;
 
 namespace Test_CRMGuru
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void textBox_Country_Click(object sender, EventArgs e)
+        {
+            if (textBox_Country.Text == "Введите название страны")
+            {
+                textBox_Country.Clear();
+                textBox_Country.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox_Country_Leave(object sender, EventArgs e)
+        {
+            if (textBox_Country.Text == "")
+            {
+                textBox_Country.ForeColor = Color.Gray;
+                textBox_Country.Text = "Введите название страны";                
+            }
         }
     }
 }
